e73b4786f775137fdc07af6e10f2122419e8bf8f
redayboss7 <redayboss7@gmail.com>
2026-06-15 10:31:11 +0600

    blade_tampleing

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/dashboard', function () {
+    return view('backend.dashboard');
+});
+
